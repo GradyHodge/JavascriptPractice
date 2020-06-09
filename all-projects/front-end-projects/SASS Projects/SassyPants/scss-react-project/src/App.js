138019222a1a907f74d9984719aef08e1ebba9b1
Grady Hodge <gradywhodge@gmail.com>
2020-06-09 13:40:40 -0500

    updates on content and layout

---
+++ all-projects/front-end-projects/SASS Projects/SassyPants/scss-react-project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn">This is my button</button>
+    </div>
+  );
+}
+
+export default App;
